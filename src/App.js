7408849879a6de5561e51a5a85e074364c0e272f
Luke Pritchard <luke-pritch@users.noreply.github.com>
2021-02-26 12:49:53 -0500

    Adding icons

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { Admin, ListGuesser, Resource, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import UserList from './components/UsersList';
 import PostList from './components/PostList';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import TodoIcon from '@material-ui/icons/DoneOutline';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name='users' list={UserList} edit={EditGuesser} />
-    <Resource name='posts' list={PostList} edit={EditGuesser} />
-    <Resource name='todos' list={ListGuesser} edit={EditGuesser} />
+    <Resource name='users' list={UserList} edit={EditGuesser} icon={UserIcon} />
+    <Resource name='posts' list={PostList} edit={EditGuesser} icon={PostIcon} />
+    <Resource
+      name='todos'
+      list={ListGuesser}
+      edit={EditGuesser}
+      icon={TodoIcon}
+    />
   </Admin>
 );
 
