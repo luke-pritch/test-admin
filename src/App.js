36e077772232f6c35be24f44d7bc77032880a739
Luke Pritchard <luke-pritch@users.noreply.github.com>
2021-02-26 12:14:34 -0500

    init react admin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import UserList from './components/UsersList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name='users' list={UserList} />
+    <Resource name='posts' list={ListGuesser} />
+    <Resource name='comments' list={ListGuesser} />
+    <Resource name='photos' list={ListGuesser} />
+    <Resource name='todos' list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
---
+++ src/components/UsersList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Datagrid, EmailField, List, TextField, UrlField } from 'react-admin';
+import MyUrlField from './fields/MyUrlField';
+
+const UserList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick='edit'>
+      <TextField source='id' />
+      <TextField source='name' />
+      <EmailField source='email' />
+      <TextField source='phone' />
+      <MyUrlField source='website' />
+      <TextField source='company.name' />
+    </Datagrid>
+  </List>
+);
+
+export default UserList;
